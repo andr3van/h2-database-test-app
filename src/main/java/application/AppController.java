09e28425ecad6061a09ede36270da286e78ecf77
andrevan <a_evan@kinsoku.net>
2020-02-05 13:49:37 +0900

    UI simplified and enhanced

--- src/main/java/application/AppController.java
+++ src/main/java/application/AppController.java
@@ -1,8 +1,8 @@
 package application;
 
 import database.Connectivity;
-import database.Create;
-import database.Record;
+import database.CreateTable;
+import database.InsertRecord;
 import static javafx.collections.FXCollections.observableArrayList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -21,26 +22,43 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class AppController implements Initializable {
+
+    @FXML
+    private Label connStat;
+    @FXML
+    private Button connect;
+    @FXML
+    private Button disconnect;
+    @FXML
+    private Button tableCreate;
+    @FXML
+    private Button tableDelete;
+    @FXML
+    private Button recordInsert;
+    @FXML
+    private Button recordDelete;
     @FXML
     private Button exit;
     @FXML
     private TextField tableNameField;
     @FXML
-    private ComboBox<String> tableRecord;
+    private TextField recordField;
     @FXML
-    private TextField dataField;
+    private ComboBox<String> tableList;
     @FXML
-    private ComboBox<String> tableForDelete;
-    @FXML
-    private ComboBox<String> tableToRecord;
-    @FXML
-    private ComboBox<String> recordForDelete;
+    private ComboBox<String> recordList;
 
     private static AppController instance;
 
+    private final String disconnectedLabel = "Currently not connected...";
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        tableToRecord.valueProperty().addListener((observable, oldValue, newValue) -> {
+        connStat.setText(disconnectedLabel);
+
+        disableButtons(true);
+
+        tableList.valueProperty().addListener((observable, oldValue, newValue) -> {
             try {
                 recordPooler(newValue);
             } catch (SQLException e) {
@@ -61,14 +79,32 @@ public class AppController implements Initializable {
     }
 
     @FXML
-    private void onConnect() throws SQLException {
-        Connectivity.INSTANCE.dbConnect();
-        tablePooler();
+    private void onConnect()  {
+        try {
+            Connectivity.INSTANCE.dbConnect();
+            tablePooler();
+
+            String connectedLabel = "Connected...";
+            connStat.setText(connectedLabel);
+
+            disableButtons(false);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     private void onDisconnect() {
         Connectivity.INSTANCE.dbDisconnect();
+        connStat.setText(disconnectedLabel);
+
+        //clear data
+        tableNameField.clear();
+        recordField.clear();
+        tableList.getItems().clear();
+        recordList.getItems().clear();
+
+        disableButtons(true);
     }
 
     @FXML
@@ -79,9 +115,10 @@ public class AppController implements Initializable {
     @FXML
     private void onTableCreate() {
         try {
-            Create cr = new Create();
+            CreateTable cr = new CreateTable();
             cr.createTable(tableNameField.getText());
 
+            tableNameField.clear();
             tablePooler();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -91,8 +128,8 @@ public class AppController implements Initializable {
     @FXML
     private void onTableDelete() {
         try {
-            Create cr = new Create();
-            cr.deleteTable(tableForDelete.getValue());
+            CreateTable cr = new CreateTable();
+            cr.deleteTable(tableList.getValue());
 
             tablePooler();
         } catch (SQLException e) {
@@ -103,10 +140,11 @@ public class AppController implements Initializable {
     @FXML
     private void onRecordCreate() {
         try {
-            Record rec = new Record();
-            rec.createRecord(tableRecord.getValue(), dataField.getText());
+            InsertRecord rec = new InsertRecord();
+            rec.createRecord(tableList.getValue(), recordField.getText());
 
-            recordPooler(tableToRecord.getValue());
+            recordField.clear();
+            recordPooler(tableList.getValue());
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -115,10 +153,10 @@ public class AppController implements Initializable {
     @FXML
     private void onRecordDelete() {
         try {
-            Record rec = new Record();
-            rec.deleteRecord(tableToRecord.getValue(), recordForDelete.getValue());
+            InsertRecord rec = new InsertRecord();
+            rec.deleteRecord(tableList.getValue(), recordList.getValue());
 
-            recordPooler(tableToRecord.getValue());
+            recordPooler(tableList.getValue());
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -133,14 +171,12 @@ public class AppController implements Initializable {
         // Extract data from result set
         ArrayList<String> dataList = new ArrayList<>();
         while (rs.next()) {
-            // Retrieve by column name
+            // Retrieve by table name
             String data = rs.getString("TABLE_NAME");
             dataList.add(data);
         }
 
-        tableRecord.setItems(observableArrayList(dataList));
-        tableForDelete.setItems(observableArrayList(dataList));
-        tableToRecord.setItems(observableArrayList(dataList));
+        tableList.setItems(observableArrayList(dataList));
     }
 
     public void recordPooler(String table) throws SQLException {
@@ -150,15 +186,24 @@ public class AppController implements Initializable {
             ResultSet rs = Connectivity.INSTANCE.getStmt().executeQuery(sql);
 
             // Extract data from result set
-            ArrayList<String> recordList = new ArrayList<>();
+            ArrayList<String> recList = new ArrayList<>();
             while (rs.next()) {
                 // Retrieve by column name
                 String rec = rs.getString("data");
-                recordList.add(rec);
+                recList.add(rec);
             }
 
-            recordForDelete.setItems(observableArrayList(recordList));
+            recordList.setItems(observableArrayList(recList));
         }
+    }
+
+    public void disableButtons(boolean bool) {
+        connect.setDisable(!bool);
+        disconnect.setDisable(bool);
+        tableCreate.setDisable(bool);
+        tableDelete.setDisable(bool);
+        recordInsert.setDisable(bool);
+        recordDelete.setDisable(bool);
     }
 
 }
